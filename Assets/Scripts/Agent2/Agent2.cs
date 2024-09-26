8e2bdd64fbfd967b099809dc4defa5bda1f0a027
Julius Kirsch <Julius@skirsch.de>
2024-09-26 22:37:52 +0200

    feat: map2 jump on block and jump again

--- Assets/Scripts/Agent2/Agent2.cs
+++ Assets/Scripts/Agent2/Agent2.cs
@@ -8,23 +8,25 @@ using UnityEngine;
 public class Agent2 : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 5;
-    [SerializeField] float rotateSpeed = 20;
+    [SerializeField] float rotateSpeed = 50;
+    [SerializeField] float jumpHeight = 5;
     [SerializeField] float epsilonReductionRate = 0.001f;
     [SerializeField] float epsilon = 1f;
     [SerializeField] float epsilonMin = 0.01f;
     [SerializeField] float gamma = 0.9f;
     [SerializeField] int numEpisodes = 1000;
-
     [SerializeField] GameObject Player;
-    [SerializeField] GameObject Obstacle;
+    [SerializeField] GameObject Box1;
+    [SerializeField] GameObject Obstacle2;
     [SerializeField] GameObject Goal;
     [SerializeField] GameObject Floor;
     [SerializeField] TMP_Text overviewDisplay;
     [SerializeField] TMP_Text epsilonDisplay;
 
+    bool hitBox = false;
     bool hitGoal = false;
     bool hitObstacle = false;
-    bool hitCheckPoint = false;
+    bool isGrounded = false;
     int trainedEpochs = 0;
     NNModel model;
     bool done = false;
@@ -33,14 +35,15 @@ public class Agent2 : MonoBehaviour
     int passedCount = 0;
     int failedCount = 0;
     float passedFailedRatio = 0.0f;
+    float totalTime = 0;
 
     void Start()
     {
         //Time.timeScale = 5;
         model = NetworkBuilder.Create()
-            .Stack(new InputLayer(4)) //left-right-
-            .Stack(new DenseLayer(20, ActivationType.Sigmoid))
-            .Stack(new OutputLayer(3, ActivationType.Sigmoid))
+            .Stack(new InputLayer(4)) //obst1 distance, obst2 distance, rotationY
+            .Stack(new DenseLayer(50, ActivationType.Sigmoid))
+            .Stack(new OutputLayer(2, ActivationType.Sigmoid))
             .Build(false);
 
         initialPlayerPosition = Player.transform.position;
@@ -98,6 +101,16 @@ public class Agent2 : MonoBehaviour
         Player.transform.Rotate(new Vector3(0, -1, 0) * rotateSpeed * Time.deltaTime);
     }
 
+    void JumpPlayer()
+    {
+        //jump only if the player is grounded and not in cooldown
+        if (isGrounded)
+        {
+            Player.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
     IEnumerator TrainModel()
     {
         for (int episode = 0; episode < numEpisodes; episode++)
@@ -109,38 +122,45 @@ public class Agent2 : MonoBehaviour
             {
                 float reward = 0;
                 int action = 0;
-                float frontDist = Player.transform.position.x - Obstacle.transform.position.x;
-                float sideDist = frontDist < 1 ? Obstacle.transform.position.y - Player.transform.position.y : -1;
-                float leftDist = sideDist < 0 ? (-sideDist) : -1;
-                float rightDist = sideDist > 0 ? sideDist : -1;
+                float boxDist = Player.transform.position.x - Box1.transform.position.x;
+                float obst2Dist = Player.transform.position.x - Obstacle2.transform.position.x;
                 bool fellDown = Player.transform.position.y - Floor.transform.position.y < 0;
                 float rotationY = Player.transform.rotation.y;
-                float[] state = { frontDist, leftDist, rightDist, rotationY};
+                float height = Player.transform.position.y - initialPlayerPosition.y;
 
-                epsilonDisplay.text = frontDist + "";
+                float[] state = { boxDist, obst2Dist, height, Time.time - totalTime };
 
                 if (Random.value < epsilon)
-                    action = Random.Range(0, 3);
+                    action = Random.Range(0, 1);
                 else
                     action = ArgsMaxIndex(model.Predict(state));
 
-                if (action == 1)
-                    RotateLeft();
-                else if (action == 2)
-                    RotateRight();
+                //if (action == 1)
+                //    RotateRight();
+                //else if (action == 2)
+                //    RotateLeft();
+                if(action == 1)
+                    JumpPlayer();
 
-                Debug.Log("Train with: Forward: " + state[0] + " Left: " + state[1] + "Right: " + state[2]);
+                Debug.Log("Train with: Forward: " + state[0] + " Rotation: " + state[1]);
 
                 if (fellDown)
                 {
                     reward = -1f;
                     failedCount++;
+                    done = true;
+                }
+
+                if (hitBox)
+                {
+                    reward = 0.05f;
+                    hitBox = false;
                 }
 
                 //check for goal and obstacle conditions
                 if (hitGoal)
                 {
-                    reward = 0.1f;
+                    reward = (1 - Time.time - totalTime / 10) + 0.05f;
                     passedCount++;
                     hitGoal = false;
                 }
@@ -167,7 +187,9 @@ public class Agent2 : MonoBehaviour
                     if(epsilon > epsilonMin)
                         epsilon -= epsilonReductionRate;
 
-                    //epsilonDisplay.text = $"Epsilon: {epsilon}";
+                    epsilonDisplay.text = $"Epsilon: {epsilon}";
+
+                    totalTime = Time.time;
 
                     if (done)
                     {
@@ -191,10 +213,20 @@ public class Agent2 : MonoBehaviour
             hitGoal = true;
             done = true;
         }
-        else if (collision.gameObject == Obstacle)
+        else if (collision.gameObject == Obstacle2)
         {
             hitObstacle = true;
             done = true;
+        }
+        else if(collision.gameObject == Box1)
+        {
+            hitBox = true;
+        }
+
+        //detect collision with ground for jumping
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
     }
 
